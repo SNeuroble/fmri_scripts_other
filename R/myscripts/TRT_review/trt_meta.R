dc20c78c2b1ff684d32e81980bb7b0cee81d674d
SNeuroble <stephanie.noble@yale.edu>
2019-07-22 14:00:01 -0400

    updated meta-analysis script

--- R/myscripts/TRT_review/trt_meta.R
+++ R/myscripts/TRT_review/trt_meta.R
@@ -1,75 +1,133 @@
 ###############################################################################
 #
-# Used for performing meta-analysis in "A decade of test-retest reliability of
-# functional connectivity: a systematic review and meta-analysis"
+# Used for performing quantitative summary and meta-analysis in Noble et al.
+# "A decade of test-retest reliability of functional connectivity: a systematic
+# review and meta-analysis"
 #
 ###############################################################################
 
-install.packages("metafor")
-install.packages("mixtools")
-install.packages("gdata")
+##### INSTALL LIBRARIES #####
+
+# install.packages("metafor")
+# install.packages("mixtools")
+# install.packages("gdata")
+# install.packages("foreach")
 library(metafor)
 library(mixtools)
 library(gdata)
+library(foreach)
+
+##### DEFINE FUNCTIONS #####
+
+# ICC variance approximation for ICC(1,1)
+# k is number observations of each object, n is number objects
+# assumes normality (this becomes worse closer to 1)
+# Reference: Shoukri et al., 2016 - Bias and Mean Square Error of Reliability Estimators under the One and Two Random Effects Models: The Effect of Non-Normality http://file.scirp.org/Html/5-1240660_65865.htm
+# This references Eqn 6.1 in Donner 1986 (A Review of Inference Procedures for the Intraclass Correlation Coefficient in the One-Way Random Effects Model)
+calc_icc_var <- function(icc,n,k) {
+  return(  2 * (1-icc)^2 * ( 1 + (n-1) * icc )^2 / ( k * n * (n-1) )  )
+}
 
 ######### SETUP #########
 
 # Define files
-data_path="~/"
-data_filename=paste(data_path,"detailed summary.xlsx",sep="")
-forest_plot_filename=paste(data_path,"forest.pdf",sep="")
-funnel_plot_filename=paste(data_path,"funnel.pdf",sep="")
-mix_plot_filename=paste(data_path,"density.pdf",sep="")
+data_path <- "/Volumes/GoogleDrive/My Drive/Steph - Lab/TRT Review/literature/culling papers"
+figs_path <- "/Volumes/GoogleDrive/My Drive/Steph - Lab/TRT Review/figures"
+data_filename <- paste(data_path,"/2 for meta and review.xlsx",sep="")
+forest_plot_filename <- paste(figs_path,"/forest.pdf",sep="")
+funnel_plot_filename <- paste(figs_path,"/funnel.pdf",sep="")
+mix_plot_filename <- paste(figs_path,"/density.pdf",sep="")
 
 # Import data
-d=read.xls(data_filename, sheet=2, skip=1)
-mydat=d[,c("Authors","Subjects","Year","Duration..min.","Repetition..sessions.","Anatomical.Scope","ICC.Type","ICC.Mean","ICC.SD")]
-mydat$ICC.Var=mydat$ICC.SD^2
+d <- read.xls(data_filename, sheet=2, skip=1)
+mydat <- d[,c("Authors","Dataset","Subjects","Year","Duration..min.","Inter.Session.Interval","Repetition..sessions.","Anatomical.Scope","Node.Resolution","ICC.Type","ICC.Mean","ICC.SD")]
 
-# Simple data summary
-m=median(mydat$Subjects); i=IQR(mydat$Subjects)/2; m; m-i/2; m+i/2
-m=median(mydat$Repetition..sessions.); i=IQR(mydat$Repetition..sessions.); m; m-i/2; m+i/2
-m=median(mydat$Duration..min.); i=IQR(mydat$Duration..min.); m; m-i/2; m+i/2
+# Parse variables for moderator analysis
+# - define anatomical scope
+levels(mydat$Anatomical.Scope)[!(levels(mydat$Anatomical.Scope)=='WB' | levels(mydat$Anatomical.Scope)=='DMN')] <- 'Mix' # change to WB, DMN, or mix
+# - redefine scan duration if using average measures
+mydat[c(mydat$Authors=="Shah et al."),"Duration..min."] <- mydat[c(mydat$Authors=="Shah et al."),"Duration..min."]*4
+mydat[c(mydat$Authors=="Lin et al."),"Duration..min."] <- mydat[c(mydat$Authors=="Lin et al."),"Duration..min."]*3
+# - extract inter-session intervals
+temp <- vector(mode = "list", length = length(mydat$Inter.Session.Interval))
+for (i in 1:length(mydat$Inter.Session.Interval)) {
+  t <- strsplit(as.character(mydat$Inter.Session.Interval[i])," \\(")
+  temp[i]<-t[[1]][1]
+}
+mydat$Inter.Session.Interval <- as.factor(unlist(temp))
+mydat$Inter.Session.Interval[which(mydat$Inter.Session.Interval=="<not given>")]=NA
+mydat$Inter.Session.Interval[which(mydat$Inter.Session.Interval=="medium-mixed")]=NA
+
+######## STUDY CHARACTERISTICS #########
+
+# Summarize numerical study characteristics
+for (category in c('Subjects','Repetition..sessions.','Duration..min.','ICC.Mean')) {
+  m <- median(mydat[,category]);
+  i <- IQR(mydat[,category])/2;
+  r <- range(mydat[,category]);
+  min <- min(mydat[,category]);
+  max <- max(mydat[,category]);
+  print(paste(category,' Summary: Median=',m,', IQR=',m-i/2,' - ',m+i/2,', range=',r[1],' - ',r[2],', min=',min,', max=',max,sep=""))
+}
 
 ######### META-ANALYSIS #########
-# Estimate ICC
-# http://www.metafor-project.org/doku.php/plots
-# weighting via Hunter-Schmidt - http://www.metafor-project.org/doku.php/tips:hunter_schmidt_method
 
-# Estimate as correlation via metafor
-resMeta<-rma(ri=ICC.Mean,ni=Repetition..sessions.*Subjects,data=mydat,measure="COR",method="HS")
+# Estimate ICC variance
+mydat$ICC.Var_Est <- foreach(i=1:nrow(mydat), .combine = 'rbind') %do% calc_icc_var(mydat$ICC.Mean[i], mydat$Subjects[i], mydat$Repetition..sessions.[i])
+
+# Meta-analysis: pooled estimate of ICC nested by dataset
+# nesting reference: https://bookdown.org/MathiasHarrer/Doing_Meta_Analysis_in_R/fitting-a-three-level-model.html
+resMeta <- rma.mv(ICC.Mean, ICC.Var_Est, data=mydat,
+                random = list( ~1 | Dataset ),
+                method="REML")
+print(summary(resMeta))
 
 # Estimate as arithmetic mean
-weight=mydat$Repetition..sessions.*mydat$Subjects
-wm<-weighted.mean(mydat$ICC.Mean, weight) # mean
-weighted.mean(abs(mydat$ICC.Mean-wm), weight) # SD of mean
-wm_sd<-weighted.mean(mydat$ICC.SD, weight) # SD
-weighted.mean(abs(mydat$ICC.SD-wm_sd), weight) # SD of SD
+study_weights <- mydat$Repetition..sessions.*mydat$Subjects
+for (category in c('ICC.Mean','ICC.SD')) {
+  wm <- weighted.mean(mydat[,category], study_weights, na.rm=T)
+  sd <- weighted.mean(abs(mydat[,category]-wm), study_weights, na.rm=T)
+  print(paste(category,': weighted mean=',wm,', SD=',sd,sep=""))
+}
 
-# Report & visualize meta results
-summary(resMeta)
+######### VISUALIZATION #########
 
 # Forest plot
-pdf(forest_plot_filename) 
-forest(resMeta,slab=paste(mydat$Authors,mydat$Year,sep=", "))
+pdf(forest_plot_filename)
+forest(resMeta, slab=paste(mydat$Authors,mydat$Year,sep=", ")) 
+op <- par(cex = 1, font = 2)
+text(-1.8, 27.1, "Authors and Year", pos = 4)
+text(2.6,27.1, "ICC [95% CI]", pos = 2)
+par(op)
 dev.off()
 
-# Estimate publication bias
-regtest(resMeta) # funnel regression 
-ranktest(resMeta) # funnel rank regression
-
+# Funnel plot
 pdf(funnel_plot_filename) 
 funnel(resMeta)
 dev.off()
 
-######### MIXTURE MODEL #########
+# Estimate publication bias via funnel plot asymmetry
+print(ranktest(resMeta))
+
+######### EXPLORATORY ANALYSES #########
+
+# 1. Exploratory meta-analysis with moderation by study characteristics (one at a time)
+metaMods <- colnames(mydat[,2:10])
+for (category in metaMods) {
+  resMeta_mods <- rma.mv(ICC.Mean, ICC.Var_Est, mods = cbind(get(category)), data=mydat,
+                       random = list( ~1 | Dataset ),
+                       method="REML")
+  print(paste(category,' p value: ', resMeta_mods$pval[2],' beta: ', resMeta_mods$beta[2],sep=""))
+}
+
+
+# 2. Fit Gaussian Mixture Model
 # https://www.r-bloggers.com/fitting-mixture-distributions-with-the-r-package-mixtools/
 
-# Fit Gaussian Mixture Model
 resMix <- normalmixEM(mydat$ICC.Mean)
 
 # Report mixture results
-summary(resMix)
+print(summary(resMix))
 
 # Simple Kolmogorov-Smirnov test
 # https://stats.stackexchange.com/questions/28873/goodness-of-fit-test-for-a-mixture-in-r
@@ -77,16 +135,20 @@ pmnorm <- function(x, mu, sigma, pmix) {
   pmix[1]*pnorm(x,mu[1],sigma[1]) + (1-pmix[1])*pnorm(x,mu[2],sigma[2])
 }
 KStest <- ks.test(mydat$ICC.Mean, pmnorm, mu=resMix$mu, sigma=resMix$sigma, pmix=resMix$lambda)
-KStest
+print(KStest)
 
 # Visualize mixture results
 pdf(mix_plot_filename) 
-plot(resMix,whichplots=2,xlim=c(0,0.6))  # histogram + estimated mixture densities
-lines(density(mydat$ICC.Mean), lty=2, lwd=2) # histogram density
+plot(resMix,whichplots=2,xlim=c(0,0.6))  # plot 2 = histogram + estimated mixture densities
+lines(density(mydat$ICC.Mean), lty=2, lwd=2) # add histogram density
 dev.off()
 
-# Cite
+######### CITATIONS #########
+
 citation()
 citation("metafor")
 citation("mixtools")
 citation("gdata")
+
+
+
---
+++ README.md
@@ -0,0 +1,42 @@
+# Stephanie's fMRI Analysis Repository - R and Python
+
+The contents are standalone scripts for fMRI analysis and a few other general tasks in R and Python.
+
+## Getting Started
+
+### Prerequisites
+
+R or Python. Hasn't been tested across versions.
+
+### Overview
+
+Some of these materials have been developed to conduct the core analyses for a project and are described below.
+
+
+#### 1. Meta-analysis of test-retest reliability in functional connectivity
+
+*Location:* *R > myscripts > TRT\_review*
+
+*Purpose:* Perform meta-analysis of ICC studies 
+
+*Usage:* Data for this study is given in Table 1 of the manuscript. Use this instead of path given in  *trt\_meta*.
+
+(Reference forthcoming) 
+
+
+#### 2. Multisite test-retest reliability in functional connectivity - visualization
+
+*Location:* *R > myscripts > TRT\_multisite*
+
+*Purpose:* Visualize ICC distributions. A kludge-y solution to making asymmetric violin plots. Main code is *main.R*.
+See *fmri\_scripts\_matlab* repository for calculating Generalizability Theory ICCs.
+
+*Reference:* Noble, S., Scheinost, D., Finn, E. S., Shen, X., Papademetris, X., McEwen, S. C., ... & Mirzakhanian, H. (2017). Multisite reliability of MR-based functional connectivity. Neuroimage, 146, 959-970. 
+
+
+## Contact
+
+Please let me know if you have any questions about the materials provided here! Although I try to make all my code publicly available, some of the materials in this repository have been developed for more private use and may therefore require further explanation.
+
+
+
